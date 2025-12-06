416b5dde0b3bb283ce621e9d4257ea1eb07e74f8
Akhilesh N Yadav <akki.592.148@Akkis-MacBook-Pro.local>
2025-12-06 23:01:29 +0530

    Setting up the express server and the typescript compatibility.

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, {Request, Response} from 'express';
+
+const app = express();
+const port = 592;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("This is the home page of the codello.");
+})
+
+app.get('/profile', (req: Request, res: Response) => {
+    res.send("This is the profile page of the codello.");
+})
+
+
+app.listen(port, () => {
+    console.log("Codello server is up and running.")
+})
