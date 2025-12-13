c3ff09c6c546fb9b09ac7a7955fa39056c729e19
Akhilesh N Yadav <akki.592.148@Akkis-MacBook-Pro.local>
2025-12-13 12:45:02 +0530

    playing with rout handlers and multiple route handler in single route

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,36 @@
-import express, {Request, Response} from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 const port = 592;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("This is the home page of the codello.");
-})
+  res.send('This is the home page of the codello.');
+});
 
-app.get('/profile', (req: Request, res: Response) => {
-    res.send("This is the profile page of the codello.");
-})
-
-app.post('/profile', (req: Request, res: Response) => {
-    res.send("The request to save data of the profile to the database is successfully done.")
-})
-
-app.delete('/profile', (req: Request, res: Response) => {
-    res.send("The request to delete data of the profile to the database is successfully done.")
-})
-
-app.patch('/profile', (req: Request, res: Response) => {
-    res.send("The request to edit data of the profile to the database is successfully done.")
-})
-
-
+app.get(
+  '/profile',
+  [
+    (req: Request, res: Response, next: NextFunction) => {
+      res.send('This is the profile page of the codello. 1');
+      next();
+    },
+    (req: Request, res: Response, next: NextFunction) => {
+      res.send('This is the profile page of the codello. 2');
+      next();
+    },
+  ],
+  (req: Request, res: Response, next: NextFunction) => {
+    res.send('This is the profile page of the codello. 3');
+    next();
+  },
+  (req: Request, res: Response) => {
+    res.send('This is the next profile page of the codello. 4');
+  },
+);
 
 app.listen(port, () => {
-    console.log("Codello server is up and running at - ", `http://localhost:${port}/`)
-})
+  console.log(
+    'Codello server is up and running at - ',
+    `http://localhost:${port}/`,
+  );
+});
