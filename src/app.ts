e10c33ac3cc71491d46bfa0f873137e6afadf680
Akhilesh N Yadav <akki.592.148@Akkis-MacBook-Pro.local>
2025-12-13 14:15:31 +0530

    playing with rout handlers and multiple route handler in single route

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,19 @@ const port = 592;
 app.use('/admin', adminAuth);
 
 app.get('/admin/getAllData', (req: Request, res: Response) => {
+  throw new Error();
   res.send('Admin get all data.');
 });
 
 app.get('/user', userAuth, (req: Request, res: Response) => {
   res.send('User data send.');
+});
+
+app.use('/', (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err) {
+    console.log(err);
+    res.status(500).send('Something wen wrong!!!');
+  }
 });
 
 app.listen(port, () => {
