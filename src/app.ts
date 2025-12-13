81eed737f52e700ae2c880c2920ba6450d07f826
Akhilesh N Yadav <akki.592.148@Akkis-MacBook-Pro.local>
2025-12-13 17:27:17 +0530

    adding the data from the api, with post api

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import express, { NextFunction, Request, Response } from 'express';
 
 import connectCluster from './configs/database';
+import User from './models/user';
 
 const app = express();
 const port = 592;
+
+app.post('/signup', async (req: Request, res: Response) => {
+  const user = new User({
+    firstName: "akhilesh",
+    lastName: "Yadav",
+    email: "akhilesh@yadav.com",
+    password: "akhilesh@24",
+    age: 26,
+    gender: "m"
+  })
+
+  await user.save();
+
+  res.send("User Added To the database.")
+})
 
 connectCluster()
   .then(() => {
--- src/configs/database.ts
+++ src/configs/database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectCluster = async () => {
   await mongoose.connect(
-    'mongodb+srv://akhileshkumaryadav2407_db_user:EBGTAnoEdYqaY900@cluster0.eintc6k.mongodb.net/',
+    'mongodb+srv://akhileshkumaryadav2407_db_user:EBGTAnoEdYqaY900@cluster0.eintc6k.mongodb.net/codello',
   );
 };
 
