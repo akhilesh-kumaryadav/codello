41e4dafeb27f06d44a4debf60980d7442fd07d6f
Akhilesh N Yadav <akki.592.148@Akkis-MacBook-Pro.local>
2026-01-27 21:53:36 +0530

    feat: user request received api

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,73 +1,35 @@
 import express, { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
+import { userAuth } from '../middlewares/auth';
+import { UserDocument } from '../models/collectionTypes/user';
 
-import signUpValidator from '../utils/signUpValidator';
-import Users from '../models/user/user';
-import loginInValidator from '../utils/loginInValidator';
+import Requests from '../models/request/request';
 
 const route = express.Router();
 
-route.post('/signup', async (req: Request, res: Response) => {
-  try {
-    signUpValidator({ bodyParams: req.body });
+route.get(
+  '/user/request/received',
+  userAuth,
+  async (req: Request, res: Response) => {
+    try {
+      const user = req.user as UserDocument;
 
-    const { firstName, lastName, email, password, gender, age, photoUrl } =
-      req.body;
+      const requestRecieved = await Requests.find({
+        toUserId: user._id,
+        status: 'interested',
+      }).populate('fromUserId', ['firstName', 'lastName']);
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = new Users({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-      gender,
-      age,
-      photoUrl,
-    });
-
-    await user.save();
-
-    res.send('User Added To the database.');
-  } catch (error: any) {
-    res.status(400).send('Error in saving the user: ' + error.message);
-  }
-});
-
-route.post('/login', async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
-  try {
-    loginInValidator({ credentials: req.body });
-
-    const user = await Users.findOne(
-      { email },
-      {
-        password: 1,
-      },
-    );
-    if (!user) {
-      throw new Error('Invalid credentials');
+      res.json({
+        status: true,
+        message: `Request Received.`,
+        data: requestRecieved,
+      });
+    } catch (error: any) {
+      res.json({
+        status: 400,
+        message: `Error in getting the request received: ${error.message}`,
+      });
     }
-
-    const isPasswordValid = await user.verifyPassword(password);
-    if (!isPasswordValid) {
-      throw new Error('Invalid credentials');
-    }
-
-    const token = await user.getJWT();
-    res.cookie('token', token, { expires: new Date(Date.now() + 900000) });
-
-    res.send('Login successfully!!!');
-  } catch (error: any) {
-    res.status(400).send(error.message ?? 'Something went wrong.');
-  }
-});
-
-route.post('/logout', (req: Request, res: Response) => {
-  res
-    .cookie('token', null, { expires: new Date(Date.now()) })
-    .send('Logout Successfully.');
-});
+  },
+);
 
 export default route;
