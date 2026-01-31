f12d7dc28626736bf633d67d20d75b944be8f21a
Akhilesh N Yadav <akki.592.148@Akkis-MacBook-Pro.local>
2026-01-31 13:48:09 +0530

    feat: setup the redux toolkit for store provider

--- client/README.md
+++ client/README.md
@@ -6,3 +6,6 @@
 6. Login, sign Up, and logout
 7. Axios - For api call, same as fetch, withCredentials = true
 8. CORS error - credentials: true
+9. Redux toolkit - store, provider, useDispatch, redux devTool, useSelector,
+10. useNaviagate
+11. .env, .env.devolopment, .env.production
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+
+import App from "./App.tsx";
+import { store } from "./app/store.ts";
+
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>,
+);
