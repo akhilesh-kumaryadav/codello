360fa7d05ccf1cf3d9e61e2d9f989a8813f1a99e
Akhilesh N Yadav <akki.592.148@Akkis-MacBook-Pro.local>
2026-01-31 11:56:30 +0530

    initial cors and axios setup for the api calling

--- client/src/components/Body.tsx
+++ client/src/components/Body.tsx
@@ -4,11 +4,13 @@ import Footer from "./Footer";
 
 const Body = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Outlet />
+      <div className="flex-grow">
+        <Outlet />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,5 +1,106 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("nk@securities.com");
+  const [password, setPassword] = useState("Akhilesh@24");
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:592/login",
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      console.dir({ response }, { depth: null });
+    } catch (error: any) {
+      console.log({ error });
+    }
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div className="card lg:card-side bg-base-200 shadow-sm m-10">
+        <figure className="w-100">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+            alt="Album"
+          />
+        </figure>
+        <div className="card-body">
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </g>
+            </svg>
+            <input
+              type="email"
+              placeholder="Email Address"
+              value={email}
+              required
+            />
+          </label>
+          <div className="validator-hint hidden">Enter valid email address</div>
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+            <input
+              type="password"
+              value={password}
+              required
+              placeholder="Password"
+              minLength={8}
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+            />
+          </label>
+          <p className="validator-hint hidden">
+            Must be more than 8 characters, including
+            <br />
+            At least one number <br />
+            At least one lowercase letter <br />
+            At least one uppercase letter
+          </p>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
