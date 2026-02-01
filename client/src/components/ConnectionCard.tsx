396bd51ae5849704d0a1086eda142a505bf4d637
Akhilesh N Yadav <akki.592.148@Akkis-MacBook-Pro.local>
2026-02-01 13:41:32 +0530

    feat: connections page display

---
+++ client/src/components/ConnectionCard.tsx
@@ -0,0 +1,20 @@
+const ConnectionsCard = ({ connection }) => {
+  return (
+    <li className="list-row">
+      <div>
+        <img className="size-10 rounded-box" src={connection.photoUrl} />
+      </div>
+      <div>
+        <div className="text-lg">
+          {connection.firstName + " " + connection.lastName}
+        </div>
+        <div className="text-xs uppercase font-semibold opacity-60">
+          Age: {connection.age} | Gender: {connection.gender}
+        </div>
+      </div>
+      <p className="list-col-wrap text-xs">{connection.about}</p>
+    </li>
+  );
+};
+
+export default ConnectionsCard;
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -65,12 +65,10 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <Link to="/profile" className="justify-between">
-                  Profile
-                </Link>
+                <Link to="/profile">Profile</Link>
               </li>
               <li>
-                <a>Settings</a>
+                <Link to="/connections">Connections</Link>
               </li>
               <li>
                 <Link to="/" onClick={handleLogout}>
